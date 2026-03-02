d28c3eccd6832e62509b6429239b9f3b18b97675
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2026-03-02 19:28:08 +0000

    ✨ (constants.ts): add TMDB API and image base URLs for integration with The Movie Database API.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const IMDB_BASE_URL = "https://www.imdb.com";
 export const IMDB_API_BASE_URL = "https://api.graphql.imdb.com";
 export const IMDB_TITLE_SEARCH_URL = `${IMDB_BASE_URL}/find`;
+export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
